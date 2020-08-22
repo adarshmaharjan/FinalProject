b0a88152a6d7371cb1351f930b8c0f65eca0a4a6
Abhishek <karma1997.acharya@gmail.com>
2020-08-22 09:46:06 +0545

    first commit

--- server/server.js
+++ server/server.js
@@ -1,25 +1,26 @@
-const express = require('express');
+const express = require("express");
 // const cookieParser = require("cookie-parser");
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const UserRouter = require('./routes/user.route');
-const InitiateMongoServer = require('./config/db');
+const UserRouter = require("./routes/user.route");
+const InitiateMongoServer = require("./config/db");
 
 const app = express();
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type',
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
   );
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
   next();
@@ -28,16 +29,16 @@ app.use(function (req, res, next) {
 InitiateMongoServer();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/api/user', UserRouter);
+app.use("/api/user", UserRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../client', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-  });
+app.use(express.static(path.resolve(__dirname, "../client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+});
 }
 
 app.listen(port, () => {
