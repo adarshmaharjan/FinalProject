260720c131fbbaf520a284b5c5b63be272da3110
Abhishek <karma1997.acharya@gmail.com>
2020-08-22 09:49:56 +0545

    asdf

--- server/server.js
+++ server/server.js
@@ -34,12 +34,12 @@ app.use(cors());
 
 app.use("/api/user", UserRouter);
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.resolve(__dirname, "../client", "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
 });
-}
+// }
 
 app.listen(port, () => {
   console.log(`server started in port ${port}`);
