c5f446c1fcb5bacd3e51f4247cac5c03849fd2d1
Abhishek Karmacharya <karma1997.acharya@gmail.om>
2020-08-22 13:34:27 +0545

    first commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-// const cookieParser = require("cookie-parser");
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-// const UserRouter = require('./routes/user.route');
-// const InitiateMongoServer = require('./config/db');
+const UserRouter = require('./routes/user.route');
+const InitiateMongoServer = require('./config/db');
 
 const app = express();
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Methods',
@@ -26,20 +25,20 @@ app.use(function (req, res, next) {
   next();
 });
 
-// InitiateMongoServer();
+InitiateMongoServer();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
-// app.use('/api/user', UserRouter);
+app.use('/api/user', UserRouter);
 
-// if (process.env.NODE_ENV === 'production') {
-app.use(express.static(path.resolve(__dirname, '../client', 'build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-});
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`server started in port ${port}`);
